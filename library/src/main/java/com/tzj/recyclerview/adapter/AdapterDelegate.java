c26d796e05bee8e1f2a3933d8cd33670e23bd5e2
tangzejin921 <tangzejin921@qq.com>
2018-09-13 11:22:02 +0800

    加网路判断

--- library/src/main/java/com/tzj/recyclerview/adapter/AdapterDelegate.java
+++ library/src/main/java/com/tzj/recyclerview/adapter/AdapterDelegate.java
@@ -1,5 +1,11 @@
 package com.tzj.recyclerview.adapter;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
@@ -11,38 +17,26 @@ import com.tzj.recyclerview.LayoutManager.LinearLayoutManager;
 public class AdapterDelegate extends RecyclerView.Adapter {
     private RecyclerView mRecyclerView;
 
-    //TODO 这里因为不同的LayoutManager 会导致 EmptyAdapter、NetErrAdapter、LoadingAdapter 展示有问题
-    //TODO 现在这样写导致代码很乱
-    private void setmRecyclerView(RecyclerView mRecyclerView) {
-        this.mRecyclerView = mRecyclerView;
-
-        RecyclerView.LayoutManager layoutManager = emptyAdapter.getLayoutManager();
-        if (layoutManager == null){
-            LinearLayoutManager temp = new LinearLayoutManager(mRecyclerView.getContext());
-            temp.setOrientation(RecyclerView.VERTICAL);
-            layoutManager = temp;
-        }
-        emptyAdapter.setLayoutManager(layoutManager);
-        netErrAdapter.setLayoutManager(layoutManager);
-        if (loadingAdapter!=null){
-            loadingAdapter.setLayoutManager(layoutManager);
-        }
-        //RecyclerView 设置完 layoutManager 后调用
-        RecyclerView.LayoutManager temp = mRecyclerView.getLayoutManager();
-        if (temp != layoutManager && temp instanceof ILayoutManager) {
-            adapter.setLayoutManager(temp);
-        }
-        if (currentAdapter!=adapter){
-            mRecyclerView.setLayoutManager(layoutManager);
-        }
-    }
-
     private EmptyAdapter emptyAdapter = new EmptyAdapter();
     private NetErrAdapter netErrAdapter = new NetErrAdapter();
     private LoadingAdapter loadingAdapter = new LoadingAdapter();
     private TzjAdapter adapter = new TzjAdapter();
 
     private RecyclerView.Adapter currentAdapter = loadingAdapter;
+
+    /**
+     * notifyDataChanged
+     */
+    private RecyclerView.AdapterDataObserver observer = new RecyclerView.AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            notifyDataChanged();
+        }
+    };
+    /**
+     * 网络监听
+     */
+    private NetBroadcastReceiver receiver = new NetBroadcastReceiver();
 
     public TzjAdapter getAdapter() {
         return adapter;
@@ -72,30 +66,62 @@ public class AdapterDelegate extends RecyclerView.Adapter {
     @Override
     public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
         super.onAttachedToRecyclerView(recyclerView);
-        setmRecyclerView(recyclerView);
+        setmRecyclerView(recyclerView);//setAdapter 时会调用
         registerAdapterDataObserver(observer);
+        if (recyclerView.getContext()!=null){
+            receiver.setConnect(recyclerView.getContext());
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+            recyclerView.getContext().registerReceiver(receiver,filter);
+        }
     }
 
     @Override
     public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
         super.onDetachedFromRecyclerView(recyclerView);
         unregisterAdapterDataObserver(observer);
+        if (recyclerView.getContext()!=null){
+            recyclerView.getContext().unregisterReceiver(receiver);
+        }
+    }
+    //TODO 这里因为不同的LayoutManager 会导致 EmptyAdapter、NetErrAdapter、LoadingAdapter 展示有问题
+    //TODO 现在这样写导致代码很乱
+    private void setmRecyclerView(RecyclerView mRecyclerView) {
+        this.mRecyclerView = mRecyclerView;
+
+        RecyclerView.LayoutManager layoutManager = emptyAdapter.getLayoutManager();
+        if (layoutManager == null){
+            LinearLayoutManager temp = new LinearLayoutManager(mRecyclerView.getContext());
+            temp.setOrientation(RecyclerView.VERTICAL);
+            layoutManager = temp;
+        }
+        emptyAdapter.setLayoutManager(layoutManager);
+        netErrAdapter.setLayoutManager(layoutManager);
+        if (loadingAdapter!=null){
+            loadingAdapter.setLayoutManager(layoutManager);
+        }
+        //RecyclerView 设置完 layoutManager 后调用
+        RecyclerView.LayoutManager temp = mRecyclerView.getLayoutManager();
+        if (temp != layoutManager && temp instanceof ILayoutManager) {
+            adapter.setLayoutManager(temp);
+        }
+        if (currentAdapter!=adapter){//setAdapter 时设置为其他的布局
+            mRecyclerView.setLayoutManager(layoutManager);
+        }
     }
 
-    private RecyclerView.AdapterDataObserver observer = new RecyclerView.AdapterDataObserver() {
-        @Override
-        public void onChanged() {
-            notifyDataChanged();
-        }
-    };
 
     private void notifyDataChanged() {
         loadingAdapter = null;
         RecyclerView.Adapter lastAdapter = currentAdapter;//上一次的 Adapter
-        if (adapter.getList().size() == 0) {
-            currentAdapter = emptyAdapter;
-        } else {
-            currentAdapter = adapter;
+        if (receiver.isConnect()){
+            if (adapter.getList().size() == 0) {
+                currentAdapter = emptyAdapter;
+            } else {
+                currentAdapter = adapter;
+            }
+        }else{
+            currentAdapter = netErrAdapter;
         }
         if (lastAdapter != currentAdapter && lastAdapter instanceof TzjAdapter) {
             RecyclerView.LayoutManager layoutManager = ((TzjAdapter) currentAdapter).getLayoutManager();
@@ -114,4 +140,35 @@ public class AdapterDelegate extends RecyclerView.Adapter {
     public LoadingAdapter getLoadingAdapter() {
         return loadingAdapter;
     }
+
+    class NetBroadcastReceiver extends BroadcastReceiver {
+        private boolean isConnect;
+
+        public boolean isConnect() {
+            return isConnect;
+        }
+        public void setConnect(Context connect) {
+            isConnect = connect(connect);
+        }
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+                boolean temp = connect(context);
+                if (temp != isConnect){
+                    isConnect = temp;
+                    notifyDataChanged();
+                }
+            }
+        }
+        private boolean connect(Context ctx){
+            ConnectivityManager manager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+            if (activeNetwork != null && activeNetwork.isConnected()) { // 连接上网络
+                return true;
+            } else {   // 没有连接上
+                return false;
+            }
+        }
+    }
 }
+
--- library/src/main/java/com/tzj/recyclerview/holder/NetErrHolder.java
+++ library/src/main/java/com/tzj/recyclerview/holder/NetErrHolder.java
@@ -1,12 +1,24 @@
 package com.tzj.recyclerview.holder;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
 
+import com.tzj.recyclerview.R;
 import com.tzj.recyclerview.entity.NetErr;
 
 public class NetErrHolder extends TzjViewHolder<NetErr>{
-
+    private Button button;
     public NetErrHolder(View itemView) {
         super(itemView);
+        button = bind(R.id.net_err_bt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                v.getContext().startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS)); //直接进入手机中的wifi网络设置界面
+                v.getContext().startActivity(new Intent(Settings.ACTION_SETTINGS)); //直接进入手机中设置界面
+            }
+        });
     }
 }

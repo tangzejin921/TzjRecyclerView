6009d877d031b0b3bec313728e31d26276ae43a1
tangzejin <tang_z2@archermind.com>
2021-03-09 11:00:19 +0800

    ----

--- library/src/main/java/com/tzj/listener/NoDoubleOnClickListener.java
+++ library/src/main/java/com/tzj/listener/NoDoubleOnClickListener.java
@@ -1,21 +1,53 @@
 package com.tzj.listener;
 
+import android.util.Log;
 import android.view.View;
 
 /**
- * Created by user on 2016/11/22.
  * 防止多次点击
  */
 public abstract class NoDoubleOnClickListener implements View.OnClickListener{
-    protected long preTime;
+    public static int DELAY = 350;
+    private static long lastClickTime;
     @Override
-    public void onClick(View v) {
-        long time = System.currentTimeMillis();
-        if (time-preTime>500){
-            preTime = time;
-            onMyClick(v);
+    public void onClick(View view) {
+        if (notAble()){
+            return;
         }
+        onMyClick(view);
     }
 
-    public abstract void onMyClick(View v);
+    public abstract void onMyClick(View view);
+
+    /**
+     * 是否可以点击
+     */
+    public static boolean notAble(){
+        return notAble(DELAY);
+    }
+
+    /**
+     * @param delay　上次点击后延迟的时间
+     * @return 不可以点击
+     */
+    public static synchronized boolean notAble(long delay){
+        long l = System.currentTimeMillis();
+        if (lastClickTime > l){
+            //防止时间的改变导致长时间不能点击
+            lastClickTime = l - delay;
+        }
+        if (l - lastClickTime <= delay){
+            Log.w("onClick","点击太快："+lastClickTime);
+            return true;
+        }
+        lastClickTime = l;
+        return false;
+    }
+
+    /**
+     * @param time　未来多长时间不可以点击
+     */
+    public static void setNotAble(int time){
+        lastClickTime = System.currentTimeMillis()+time;
+    }
 }
